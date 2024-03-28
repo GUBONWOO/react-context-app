d5770d172c4a9bedfb5b0857c4287563857e577a
GUBONWOO <zcwxzsx@gmail.com>
2024-03-28 16:03:04 +0900

    end of context

--- src/Components/Type.js
+++ src/Components/Type.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './Products.js';
 import Options from './Options';
 import ErrorBanner from './ErrorBanner.js';
+import { OrderContext } from '../context/OrderContext.js';
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderData, updateItemCount] = useContext(OrderContext);
+
+  // console.log('orderData,updateItemCount', orderData, updateItemCount);
 
   useEffect(() => {
     loadItems(orderType);
@@ -26,6 +30,9 @@ const Type = ({ orderType }) => {
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, orderType)
+      }
     />
   ));
   if (error) {
@@ -35,7 +42,7 @@ const Type = ({ orderType }) => {
     <div>
       <h2>주문 종류</h2>
       <p>하나의 가격</p>
-      <p> 총 가격:</p>
+      <p> 총 가격:{orderData.totals[orderType]}</p>
       <div
         style={{
           display: 'flex',
